16f9ec0e024188323a7e5800a935ef76402123f7
RCasares316 <richiecasares@gmail.com>
2026-01-26 12:40:48 -0500

    project complete. need readme

--- controllers/collection.js
+++ controllers/collection.js
@@ -10,30 +10,45 @@ const homeSneaker = async (req, res) => {
 };
 
 const addSneaker = (req, res) => {
-  res.render("collection/new.ejs");
-}
+  res.render("collection/new.ejs", {
+    errorMessage: null,
+    formData: {},
+  });
+};
 
 const postSneaker = async (req, res) => {
-  const currentUser = await User.findById(req.session.user._id);
-  const newData = req.body;
-  newData["owner"] = currentUser;
-  const newCollection = new Collection(newData);
-  await newCollection.save();
-  res.redirect("/collection");
-}
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const newData = req.body;
+    newData["owner"] = currentUser;
+    const newCollection = new Collection(newData);
+    await newCollection.save();
+    res.redirect("/collection");
+  } catch (err) {
+    // Mongoose validation error
+    if (err.name === "ValidationError") {
+      res.render("collection/new.ejs", {
+        errorMessage: "All required fields must be filled out.",
+        formData: req.body, // so the user doesn't lose what they typed
+      });
+    } else {
+      res.status(500).send(err.message);
+    }
+  }
+};
 
 const viewSneaker = async (req, res) => {
   const user = await User.findById(req.session.user._id);
   const collection = await Collection.findById(req.params.collectionId);
   res.render("collection/show.ejs", { collection: collection });
-}
+};
 
 const editSneaker = async (req, res) => {
   const user = await User.findById(req.session.user._id);
   const collection = await Collection.findById(req.params.collectionId);
-  console.log(collection)
+  console.log(collection);
   res.render("collection/edit.ejs", { collection: collection });
-}
+};
 
 const repostSneaker = async (req, res) => {
   const user = await User.findById(req.session.user._id);
@@ -41,21 +56,21 @@ const repostSneaker = async (req, res) => {
   collection.set(req.body);
   await collection.save();
   res.redirect(`/collection/${req.params.collectionId}`);
-}
+};
 
 const deleteSneaker = async (req, res) => {
   const user = await User.findById(req.session.user._id);
   const collection = await Collection.findById(req.params.collectionId);
-  await collection.deleteOne()
+  await collection.deleteOne();
   res.redirect("/collection");
-}
+};
 
 module.exports = {
   homeSneaker,
-    addSneaker,
-    postSneaker,
-    viewSneaker,
-    editSneaker,
-    repostSneaker,
-    deleteSneaker,
+  addSneaker,
+  postSneaker,
+  viewSneaker,
+  editSneaker,
+  repostSneaker,
+  deleteSneaker,
 };
